2b4bd67832675cd4dade8ac4efcce4ee1c233073
Максим Корнеев <maxim241210@gmail.com>
2019-07-09 22:08:20 +0300

    - Добавлена проверка TryParse - Добавлен метод Error text

--- Steam TM helper/Program.cs
+++ Steam TM helper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,32 +9,64 @@ namespace Steam_TM_helper
 {
     class Program
     {
+        static void Error_Text(string text) {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ResetColor();
+
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Steam TM helper");
 
-            Console.WriteLine("Steam TM helper");
+            // TASKS
 
-            // = buy
-            // + сделать Double.TryParce
             // + Сделать расчет т.е. Ввести цену за предмет и сумму на которую нужно закупится и получить кол-во предметов которые можно купить!
 
 
-            double price, balance;
-            int ItemCount;
+            double ItemPrice = 0, balance = 0;
+            int ItemCount = 0;
+            bool bal, IteP, IteC;
 
-            Console.Write("Введите ваш баланс: ");
-            balance = Convert.ToDouble(Console.ReadLine());// tryparce
-            
+            bal = IteP = IteC = true;
 
-            Console.Write("Введите цену за один предмет: ");
-            price = Convert.ToDouble(Console.ReadLine()); // tryparce
-            
-            Console.Write("Введите кол-во предметов: ");
-            ItemCount = Convert.ToInt32(Console.ReadLine());
+            while (bal == true){
+                Console.Write("Введите ваш баланс: ");
 
-            Console.WriteLine("Ваш баланс: "+balance+ " Цена за 1 предмет: "+price+" Кол-во предметов: "+ItemCount);
+                if (!Double.TryParse(Console.ReadLine(), out balance))
+                    Error_Text("Введите число в формате 1,23");     
+                
+                else if (balance != 0) bal = false;
+                else Error_Text("Введите число > 0");
+            }      
 
-            Console.WriteLine("Цена: "+price+" Кол-во: "+ItemCount+" Цена: "+ price * ItemCount);
+            while (IteP == true)
+            {
+                Console.Write("Введите цену за один предмет: ");
+
+                if (!Double.TryParse(Console.ReadLine(), out ItemPrice))
+                    Error_Text("Введите число в формате 1,23");
+
+                else if (ItemPrice != 0) IteP = false;
+                else Error_Text("Введите число > 0");
+            }
+
+
+            while (IteC == true)
+            {
+                Console.Write("Введите кол-во предметов: ");
+
+                if (!Int32.TryParse(Console.ReadLine(), out ItemCount))
+                    Error_Text("Введите целое число");
+
+                else if (ItemCount != 0) IteC = false;
+                else Error_Text("Введите число > 0");
+            }
+
+            Console.WriteLine("Ваш баланс: "+balance+ " Цена за 1 предмет: "+ItemPrice+" Кол-во предметов: "+ItemCount);
+
+            Console.WriteLine("Цена: "+ItemPrice+" Кол-во: "+ItemCount+" Цена: "+ ItemPrice * ItemCount);
 
             Console.ReadKey();
         }
