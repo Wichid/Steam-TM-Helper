515ad5a2d5d6ac2828b78d44251954d1cd741caf
Максим Корнеев <maxim241210@gmail.com>
2019-07-10 14:00:52 +0300

    Update v2 - Добавил циклы - Больше цветов для текста - Видно +15% стима

--- Steam TM helper/Program.cs
+++ Steam TM helper/Program.cs
@@ -9,64 +9,158 @@ namespace Steam_TM_helper
 {
     class Program
     {
-        static void Error_Text(string text) {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine(text);
-            Console.ResetColor();
+        static void Text_Color(string Color, string Text) {
 
+            switch (Color) {
+
+                case "Red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    break;
+
+                case "Green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    break;
+
+                case "Blue":
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    break;
+
+                case "DarkRed":
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    break;
+
+                case "DarkYellow":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    break;
+
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ОШИБКА СМЕНЫ ТЕКСТА");
+                    Console.ResetColor();
+                    break;
+            }
+
+            
+            Console.WriteLine(Text);
+            Console.ResetColor();
         }
 
         static void Main(string[] args)
         {
-            Console.Write("Steam TM helper");
+            Console.WriteLine("Steam TM helper Update v2\n");            
 
-            // TASKS
-
-            // + Сделать расчет т.е. Ввести цену за предмет и сумму на которую нужно закупится и получить кол-во предметов которые можно купить!
-
-
+             // buy
+            bool buy = true;
             double ItemPrice = 0, balance = 0;
             int ItemCount = 0;
             bool bal, IteP, IteC;
 
             bal = IteP = IteC = true;
+            // buy \
 
-            while (bal == true){
-                Console.Write("Введите ваш баланс: ");
 
-                if (!Double.TryParse(Console.ReadLine(), out balance))
-                    Error_Text("Введите число в формате 1,23");     
-                
-                else if (balance != 0) bal = false;
-                else Error_Text("Введите число > 0");
-            }      
+            Console.WriteLine("Выберите режим:\n \t1. Оффлайн режим \n \t2. Онлайн режим");
 
-            while (IteP == true)
-            {
-                Console.Write("Введите цену за один предмет: ");
+            ConsoleKey KeyChoice = Console.ReadKey(true).Key;
+            
+            switch (KeyChoice){ // Выбор режима Онлайн или Офлайн
 
-                if (!Double.TryParse(Console.ReadLine(), out ItemPrice))
-                    Error_Text("Введите число в формате 1,23");
+                case ConsoleKey.D1: // Оффлайн часть!
+                    Console.WriteLine("\nВы выбрали offline режим\n");
+                    Console.WriteLine("[OFFLINE] Выбирите что вы хотите сделать:\n \t1. Купить \n \t2. Расчет стоимости предметов\n \t3. Продать");
+                    switch (KeyChoice = Console.ReadKey(true).Key) { // Выбор Купить, Расчёт стоимости предмета, Продажа
 
-                else if (ItemPrice != 0) IteP = false;
-                else Error_Text("Введите число > 0");
+                        case ConsoleKey.D1:
+                            Console.WriteLine("[КУПИТЬ] "); // ОФОРМЛЕНИЕ!!!
+
+                            while (buy == true) // цикл покупки
+                            {
+
+                                while (IteP == true) // пррверка
+                                {
+                                    Console.Write("Введите цену за один предмет: ");
+
+                                    if (!Double.TryParse(Console.ReadLine(), out ItemPrice))
+                                        Text_Color("DarkRed", "Введите число в формате 1,23");
+
+                                    else if (ItemPrice != 0 & ItemPrice > 0) IteP = false;
+                                    else Text_Color("DarkRed", "Введите число > 0");
+                                }
+
+
+                                while (IteC == true) // пррверка
+                                {
+                                    Console.Write("Введите кол-во предметов: ");
+
+                                    if (!Int32.TryParse(Console.ReadLine(), out ItemCount))
+                                        Text_Color("Red", "Введите целое число");
+
+                                    else if (ItemCount != 0 & ItemCount > 0) IteC = false;
+                                    else Text_Color("Red", "Введите число > 0");
+                                }
+                                double PlusResult;
+
+                                Console.WriteLine("\nВведенные данные: Стоимость 1-го предмета: "+ ItemPrice +" Кол-во предметов: " + ItemCount);
+                                Console.WriteLine("Цена 1-го предмета: " + ItemPrice + " Кол-во предметов: " + ItemCount + " Итог: " + ItemPrice * ItemCount);
+                                Console.WriteLine("Для выхода в плюс лучше продать за "+ Math.Round(PlusResult = ItemPrice * 0.15 + ItemPrice, 2));
+
+                                ConsoleKey ContinueSell;
+                                Console.WriteLine("Хотите ли вы повторить? [Y\\N]");
+                                switch (ContinueSell = Console.ReadKey(true).Key){
+
+                                    case ConsoleKey.Y:
+                                        IteP = IteC = true;
+                                        break;
+
+                                    case ConsoleKey.N:
+                                        buy = false;
+                                        break;
+
+                                }
+
+                            }
+                            break;
+
+
+                           
+
+                        case ConsoleKey.D2:
+                            Console.WriteLine("[РАСЧЕТ] ");
+                            Console.WriteLine("Хотите ли вы расщитать количество предметов которое сможите купить за количемтво денег?");
+
+
+                            /*
+                            * Console.Write("Ваше число: ");
+                            * var (x, y) = (Console.CursorLeft,Console.CursorTop);
+                            * for (int i = 0 ; i <= MaxBalance; i++){
+                            *     Console.SetCursorPosition(x, y);
+                            *     Console.Write(i);
+                            * }
+                            */
+
+                            break;
+
+                        case ConsoleKey.D3:
+                            Console.WriteLine("[ПРОДАТЬ] ");
+
+
+
+                            break;
+
+                    }
+
+                    break;
+
+                case ConsoleKey.D2: // ОНЛАЙН ЧАСТЬ!
+                    Console.WriteLine("Вы выбрали online режим");
+                    Console.WriteLine("Тут неччего нет :(");
+                    // В будующем
+
+                    break;
+
             }
 
-
-            while (IteC == true)
-            {
-                Console.Write("Введите кол-во предметов: ");
-
-                if (!Int32.TryParse(Console.ReadLine(), out ItemCount))
-                    Error_Text("Введите целое число");
-
-                else if (ItemCount != 0) IteC = false;
-                else Error_Text("Введите число > 0");
-            }
-
-            Console.WriteLine("Ваш баланс: "+balance+ " Цена за 1 предмет: "+ItemPrice+" Кол-во предметов: "+ItemCount);
-
-            Console.WriteLine("Цена: "+ItemPrice+" Кол-во: "+ItemCount+" Цена: "+ ItemPrice * ItemCount);
 
             Console.ReadKey();
         }
