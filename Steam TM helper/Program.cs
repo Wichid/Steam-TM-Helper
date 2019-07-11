d6d808a3bad7e9539107a1b245685de0d0971b1b
Максим Корнеев <maxim241210@gmail.com>
2019-07-11 11:33:58 +0300

    Update v3 - Расчет стоимости предметов - мелкие правки

--- Steam TM helper/Program.cs
+++ Steam TM helper/Program.cs
@@ -33,14 +33,12 @@ namespace Steam_TM_helper
                     Console.BackgroundColor = ConsoleColor.DarkYellow;
                     break;
 
-
                 default:
                     Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("ОШИБКА СМЕНЫ ТЕКСТА");
                     Console.ResetColor();
                     break;
             }
-
             
             Console.WriteLine(Text);
             Console.ResetColor();
@@ -48,22 +46,10 @@ namespace Steam_TM_helper
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Steam TM helper Update v2\n");            
+            Console.WriteLine("Steam TM helper Update v3\n");
+            Console.WriteLine("Выберите режим:\n \t1. Оффлайн режим \n \t2. Онлайн режим \n \t3. Настройки");
 
-             // buy
-            bool buy = true;
-            double ItemPrice = 0, balance = 0;
-            int ItemCount = 0;
-            bool bal, IteP, IteC;
-
-            bal = IteP = IteC = true;
-            // buy \
-
-
-            Console.WriteLine("Выберите режим:\n \t1. Оффлайн режим \n \t2. Онлайн режим");
-
-            ConsoleKey KeyChoice = Console.ReadKey(true).Key;
-            
+            ConsoleKey KeyChoice = Console.ReadKey(true).Key;            
             switch (KeyChoice){ // Выбор режима Онлайн или Офлайн
 
                 case ConsoleKey.D1: // Оффлайн часть!
@@ -73,6 +59,11 @@ namespace Steam_TM_helper
 
                         case ConsoleKey.D1:
                             Console.WriteLine("[КУПИТЬ] "); // ОФОРМЛЕНИЕ!!!
+
+                            bool buy = true; // цикл для покупки
+                            double ItemPrice = 0;
+                            int ItemCount = 0;
+                            bool IteP = true, IteC = true; // циклы
 
                             while (buy == true) // цикл покупки
                             {
@@ -88,7 +79,6 @@ namespace Steam_TM_helper
                                     else Text_Color("DarkRed", "Введите число > 0");
                                 }
 
-
                                 while (IteC == true) // пррверка
                                 {
                                     Console.Write("Введите кол-во предметов: ");
@@ -101,8 +91,7 @@ namespace Steam_TM_helper
                                 }
                                 double PlusResult;
 
-                                Console.WriteLine("\nВведенные данные: Стоимость 1-го предмета: "+ ItemPrice +" Кол-во предметов: " + ItemCount);
-                                Console.WriteLine("Цена 1-го предмета: " + ItemPrice + " Кол-во предметов: " + ItemCount + " Итог: " + ItemPrice * ItemCount);
+                                Console.WriteLine("\nЦена 1-го предмета: " + ItemPrice + " Кол-во предметов: " + ItemCount + " Итог: " + ItemPrice * ItemCount);
                                 Console.WriteLine("Для выхода в плюс лучше продать за "+ Math.Round(PlusResult = ItemPrice * 0.15 + ItemPrice, 2));
 
                                 ConsoleKey ContinueSell;
@@ -116,47 +105,101 @@ namespace Steam_TM_helper
                                     case ConsoleKey.N:
                                         buy = false;
                                         break;
-
                                 }
 
                             }
-                            break;
-
-
-                           
+                            break; // конец цикл покупки
 
                         case ConsoleKey.D2:
                             Console.WriteLine("[РАСЧЕТ] ");
-                            Console.WriteLine("Хотите ли вы расщитать количество предметов которое сможите купить за количемтво денег?");
+                            Console.WriteLine("Расчет кол-ва предметов на сколько вам хватит");
 
+                            bool Count_Buy = true; // цикл для расчета
 
-                            /*
-                            * Console.Write("Ваше число: ");
-                            * var (x, y) = (Console.CursorLeft,Console.CursorTop);
-                            * for (int i = 0 ; i <= MaxBalance; i++){
-                            *     Console.SetCursorPosition(x, y);
-                            *     Console.Write(i);
-                            * }
-                            */
+                            while (Count_Buy == true) // цикл расчёта
+                            {
+                                 
+                                double Balance = 0, Item_Price = 0, Sec_Result = 0;
+                                int Item_Count_All = 0;
+                                bool Sec_Balance = true, Sec_Price = true, Sec_Func = true; // циклы
 
-                            break;
+                                while (Sec_Price == true) // пррверка цены
+                                {
+                                    Console.Write("Введите цену за один предмет: ");
+
+                                    if (!Double.TryParse(Console.ReadLine(), out Item_Price))
+                                        Text_Color("DarkRed", "Введите число в формате 1,23");
+
+                                    else if (Item_Price != 0 & Item_Price > 0) Sec_Price = false;
+                                    else Text_Color("DarkRed", "Введите число > 0");
+                                }
+
+                                while (Sec_Balance == true) // пррверка баланса
+                                {
+                                    Console.Write("Введите ваш бюджет: ");
+
+                                    if (!Double.TryParse(Console.ReadLine(), out Balance))
+                                        Text_Color("DarkRed", "Введите число в формате 1,23");
+
+                                    else if (Balance != 0 & Balance > 0) Sec_Balance = false;
+                                    else Text_Color("Red", "Введите число > 0");
+                                }
+
+                                Console.Write("Вы сможите купить себе: ");
+                                var (x, y) = (Console.CursorLeft, Console.CursorTop);
+                                while (Sec_Func == true) {
+
+                                    if (Balance >= Item_Price) {
+                                        Item_Count_All++;
+                                        Balance -= Item_Price;
+                                        Console.SetCursorPosition(x, y);
+                                        Console.Write(Item_Count_All +" предметов");
+                                        if (Balance < Item_Price) {
+                                            if(Balance != 0)
+                                            Console.Write(", Остаток: "+ Math.Round(Balance,2 ));
+                                        }
+                                    }
+                                    else Sec_Func = false;
+                                        
+                                }
+
+                                ConsoleKey ContinueSell;
+                                Console.WriteLine("\nХотите ли вы повторить? [Y\\N]");
+                                switch (ContinueSell = Console.ReadKey(true).Key)
+                                {
+
+                                    case ConsoleKey.Y:
+                                        Sec_Price = Sec_Balance = true;
+                                        break;
+
+                                    case ConsoleKey.N:
+                                        Count_Buy = false; // выход из цикла
+                                        break;
+
+                                }
+                            }
+                            break; // конец расчет
 
                         case ConsoleKey.D3:
                             Console.WriteLine("[ПРОДАТЬ] ");
-
-
 
                             break;
 
                     }
 
-                    break;
+                    break; // конец оффлайн часть
 
                 case ConsoleKey.D2: // ОНЛАЙН ЧАСТЬ!
                     Console.WriteLine("Вы выбрали online режим");
                     Console.WriteLine("Тут неччего нет :(");
                     // В будующем
 
+                    break;
+
+                case ConsoleKey.D3:
+                    // Насьройки
+                    Console.WriteLine("[НАСТРОЙКИ]");
+                    // Properties
                     break;
 
             }
